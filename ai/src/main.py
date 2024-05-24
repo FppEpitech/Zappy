bf676e1b54010242a38e26289a753962bbccff1f
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-05-24 18:19:09 +0200

    refactor: type of main in server from cpp to python
    Rename the file and update the main function.

---
+++ ai/src/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+##
+## EPITECH PROJECT, 2024
+## Zappy AI
+## File description:
+## main
+##
+
+
+def main():
+    print("Welcome to the AI")
+
+if __name__ == "__main__":
+    main()
