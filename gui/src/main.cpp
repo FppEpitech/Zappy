6ccfef9c07166368f745f90e14593e4d32ce11da
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-05-27 15:13:11 +0200

    fix: remove test main

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -6,26 +6,9 @@
 */
 
 #include <iostream>
-#include "Network/Network.hpp"
-#include "Parsing/ServerParser.hpp"
 
-int main()
+int main(void)
 {
-    // std::cout << "Welcome to GUI" << std::endl;
-
-    // Gui::ServerParser p;
-
-    // std::vector<std::string>a =  p.parse("msz 1 2");
-
-    // for (size_t i = 0; i < a.size(); i++)
-    //     std::cout << a[i] << std::endl;
-
-    Gui::Network net(8888, "127.0.0.1");
-    net.connectToServer();
-
-    while (1) {
-        std::cout << net.listenServer();
-    }
-
+    std::cout << "Welcome to GUI" << std::endl;
     return 0;
 }
