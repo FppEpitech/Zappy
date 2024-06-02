3a9b47a73d8d8ef99b345dfda5b09336ef6ea4be
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-06-02 10:06:42 +0200

    feat: Inventory class

---
+++ gui/include/Player/Inventory.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy GUI
+** File description:
+** Inventory
+*/
+
+#pragma once
+
+namespace Gui {
+
+    /**
+     * @brief Inventory class of a player.
+     *
+     */
+    class Inventory;
+};
+
+class Gui::Inventory {
+
+    public:
+
+        Inventory() = default;
+        ~Inventory() = default;
+
+    private:
+
+};
