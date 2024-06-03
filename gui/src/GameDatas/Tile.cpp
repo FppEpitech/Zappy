c3a1343d6951bf04c5744ea057e658a9aafed28d
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-06-03 10:10:33 +0200

    feat: Tile class

---
+++ gui/src/GameDatas/Tile.cpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy GUI
+** File description:
+** Tile
+*/
+
+#include "GameDatas/Tile.hpp"
+
+Gui::Tile::Tile(std::pair<std::size_t, std::size_t> position)
+{
+    _position = position;
+}
+
+void Gui::Tile::setPosition(std::pair<std::size_t, std::size_t> position)
+{
+    _position = position;
+}
+
+std::pair<std::size_t, std::size_t> Gui::Tile::getPosition(void)
+{
+    return _position;
+}
