b4992e8129dccb3439e335cd04568676244843db
Thomaltarix <thomas.boue@epitech.eu>
2024-06-05 00:51:10 +0200

    fix: Remove an untestable case

--- gui/tests/Event/TestEvent.cpp
+++ gui/tests/Event/TestEvent.cpp
@@ -14,13 +14,3 @@ Test(Event, constructor, .timeout = 5)
 
     cr_assert_not_null(&event);
 }
-
-Test(Event, setRender, .timeout = 5)
-{
-    Gui::Event event;
-    std::shared_ptr<Gui::GameData> gameData = std::make_shared<Gui::GameData>();
-    std::shared_ptr<Gui::Render> render = std::make_shared<Gui::Render>(gameData);
-
-    event.setRender(render);
-    cr_assert_not_null(&event);
-}
