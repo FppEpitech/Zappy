edf28c0e9f882bd1e3f692d2a0a67db038e7508f
Thomaltarix <thomas.boue@epitech.eu>
2024-06-05 19:18:37 +0200

    refactor: Player construction and add orientation to the player

--- gui/src/GameDatas/Player.cpp
+++ gui/src/GameDatas/Player.cpp
@@ -7,12 +7,8 @@
 
 #include "GameDatas/Player.hpp"
 
-Gui::Player::Player(std::size_t id, const std::string &team, std::pair<std::size_t, std::size_t> position)
+Gui::Player::Player(std::size_t id, const std::string &team, std::pair<std::size_t, std::size_t> position, std::size_t orientation, std::size_t level) : _id(id), _team(team), _position(position), _orientation(orientation), _level(level)
 {
-    _position = position;
-    _id = id;
-    _level = 1;
-    _team = team;
 }
 
 void Gui::Player::setPosition(std::pair<std::size_t, std::size_t> position)
@@ -28,6 +24,11 @@ void Gui::Player::setId(std::size_t id)
 void Gui::Player::setLevel(std::size_t level)
 {
     _level = level;
+}
+
+void Gui::Player::setOrientation(std::size_t orientation)
+{
+    _orientation = orientation;
 }
 
 void Gui::Player::setTeam(const std::string &team)
@@ -48,6 +49,11 @@ std::size_t Gui::Player::getId(void) const
 std::size_t Gui::Player::getLevel(void) const
 {
     return _level;
+}
+
+std::size_t Gui::Player::getOrientation(void) const
+{
+    return _orientation;
 }
 
 std::string Gui::Player::getTeam(void) const
