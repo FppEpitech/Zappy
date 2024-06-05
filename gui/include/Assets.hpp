d1931b15bee4e7ac35f5026a6c59fa3843b9f5c1
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-06-05 17:34:20 +0200

    feat: phiras, sibur, deraumere and thystame in Asset.hpp

--- gui/include/Assets.hpp
+++ gui/include/Assets.hpp
@@ -20,20 +20,32 @@
 
 #define SCALE_FOOD                  (Vector3){2, 2, 2}
 #define SCALE_LINEMATE              (Vector3){0.05, 0.05, 0.05}
-#define SCALE_MENDIANE              (Vector3){0.5, 0.5, 0.5}
-#define SCALE_PHIRAS                (Vector3){0.5, 0.5, 0.5}
+#define SCALE_MENDIANE              (Vector3){0.1, 0.1, 0.1}
+#define SCALE_PHIRAS                (Vector3){0.2, 0.2, 0.2}
+#define SCALE_SIBUR                 (Vector3){0.2, 0.2, 0.2}
+#define SCALE_THYSTAME              (Vector3){0.2, 0.2, 0.2}
+#define SCALE_DERAUMERE             (Vector3){0.5, 0.5, 0.5}
 
 #define ROTATION_ANGLE_FOOD         -90
 #define ROTATION_ANGLE_LINEMATE     -90
-#define ROTATION_ANGLE_MENDIANE     0
-#define ROTATION_ANGLE_PHIRAS       0
+#define ROTATION_ANGLE_MENDIANE     -90
+#define ROTATION_ANGLE_PHIRAS       180
+#define ROTATION_ANGLE_SIBUR        -90
+#define ROTATION_ANGLE_THYSTAME     0
+#define ROTATION_ANGLE_DERAUMERE    0
 
 #define ROTATION_AXIS_FOOD          (Vector3){1, 0, 0}
 #define ROTATION_AXIS_LINEMATE      (Vector3){1, 0, 0}
-#define ROTATION_AXIS_MENDIANE      (Vector3){0, 0, 0}
-#define ROTATION_AXIS_PHIRAS        (Vector3){0, 0, 0}
+#define ROTATION_AXIS_MENDIANE      (Vector3){1, 0, 0}
+#define ROTATION_AXIS_PHIRAS        (Vector3){1, 0, 0}
+#define ROTATION_AXIS_SIBUR         (Vector3){1, 0, 0}
+#define ROTATION_AXIS_THYSTAME      (Vector3){0, 0, 0}
+#define ROTATION_AXIS_DERAUMERE     (Vector3){0, 0, 0}
 
-#define POS_FOOD                    (Vector3){1, 0.1, 1}
-#define POS_LINEMATE                (Vector3){1, 0.1, -1}
-#define POS_MENDIANE                (Vector3){2, 0.1, -1}
-#define POS_PHIRAS                  (Vector3){2, 0.1, -2}
+#define POS_FOOD                    (Vector3){0.5, 0.1, 1.5}
+#define POS_LINEMATE                (Vector3){1, 0.1, -0.5}
+#define POS_MENDIANE                (Vector3){2, 0.25, -0.5}
+#define POS_PHIRAS                  (Vector3){0.5, 0.5, -1.5}
+#define POS_SIBUR                   (Vector3){1.5, 0.1, -1.5}
+#define POS_THYSTAME                (Vector3){1, 0.2, -2}
+#define POS_DERAUMERE               (Vector3){2, 0.1, -2}
