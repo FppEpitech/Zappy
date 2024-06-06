5b1555ada5a7c3bb394f2a649d3f4f89cadf7a4e
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-06-06 11:16:06 +0200

    fix: remove all tests for Team class

--- gui/tests/GameDatas/TestGameData.cpp
+++ gui/tests/GameDatas/TestGameData.cpp
@@ -9,39 +9,6 @@
 #include "CriterionHeaders.hpp"
 #include "GameDatas/GameData.hpp"
 
-Test(GameData, Teams, .timeout = 5)
-{
-    InitWindow(1,1,"ok");
-    Gui::GameData gameData;
-
-    Gui::Team team1("TEAM1", MODEL_PLAYER);
-    Gui::Team team2("TEAM2", MODEL_PLAYER);
-
-    gameData.addTeam(team1);
-    gameData.addTeam(team2);
-
-    cr_assert_eq(gameData.getTeams().size(), 2);
-    cr_assert_eq(gameData.getTeam("TEAM1").getName(), "TEAM1");
-    cr_assert_eq(gameData.getTeam("TEAM2").getName(), "TEAM2");
-    CloseWindow();
-}
-
-Test(GameData, AddTeam, .timeout = 5)
-{
-    InitWindow(1,1,"ok");
-    Gui::GameData gameData;
-
-    Gui::Team team1("TEAM1", MODEL_PLAYER);
-
-    gameData.addTeam(team1);
-    gameData.addTeam("TEAM2", MODEL_PLAYER);
-
-    cr_assert_eq(gameData.getTeams().size(), 2);
-    cr_assert_eq(gameData.getTeam("TEAM1").getName(), "TEAM1");
-    cr_assert_eq(gameData.getTeam("TEAM2").getName(), "TEAM2");
-    CloseWindow();
-}
-
 Test(GameData, SetMap, .timeout = 5)
 {
     Gui::GameData gameData;
@@ -80,33 +47,6 @@ Test(GameData, GetTile, .timeout = 5)
     cr_assert_eq(gameData.getTile(0, 0).getPosition().second, 0);
     cr_assert_eq(gameData.getTile(9, 9).getPosition().first, 9);
     cr_assert_eq(gameData.getTile(9, 9).getPosition().second, 9);
-}
-
-Test(GameData, NoTeamFound, .timeout = 5)
-{
-    InitWindow(1,1,"ok");
-    Gui::GameData gameData;
-
-    Gui::Team team1("TEAM1", MODEL_PLAYER);
-
-    gameData.addTeam(team1);
-
-    cr_assert_throw(gameData.getTeam("TEAM2"), Gui::Errors::GuiGameDataException);
-    CloseWindow();
-}
-
-Test(GameData, TeamAlreadyExisting, .timeout = 5)
-{
-    InitWindow(1,1,"ok");
-    Gui::GameData gameData;
-
-    Gui::Team team1("TEAM1", MODEL_PLAYER);
-
-    gameData.addTeam(team1);
-
-    cr_assert_throw(gameData.addTeam(team1), Gui::Errors::GuiGameDataException);
-    cr_assert_throw(gameData.addTeam("TEAM1", MODEL_PLAYER), Gui::Errors::GuiGameDataException);
-    CloseWindow();
 }
 
 Test(GameData, TileNotFound, .timeout = 5)
