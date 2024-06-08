54434a155e6eecab3a444b21a477b78316524d0f
Fradet <axel.fradet@epitech.eu>
2024-06-08 17:50:27 +0200

    feat: Fork command

--- server/src/ai/cmd/little.c
+++ server/src/ai/cmd/little.c
@@ -18,10 +18,25 @@ static void connect_nbr_cmd(app_t *app, ia_t *ai)
     add_message(ai->list_messages, reply);
 }
 
+static void fork_cmd(app_t *app, ia_t *ai)
+{
+    team_t *team = find_team(app, ai->fd);
+    char *reply = format_string("ok\n");
+
+    team->max_place += 1;
+    add_egg(team->egg_position, rand() % app->game->height,
+    rand() % app->game->width);
+    add_message(ai->list_messages, reply);
+}
+
 bool little_command(app_t *app, ia_t *ai, char *line)
 {
     if (strcmp("Connect_nbr", line) == 0) {
         connect_nbr_cmd(app, ai);
+        return true;
+    }
+    if (strcmp("Fork", line) == 0) {
+        fork_cmd(app, ai);
         return true;
     }
     return false;
