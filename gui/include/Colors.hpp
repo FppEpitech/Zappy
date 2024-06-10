eafa0725a1784fbc12d37dbe5d0643670ea637b7
Thomaltarix <thomas.boue@epitech.eu>
2024-06-10 17:29:55 +0200

    feat: Color header file for debug display

---
+++ gui/include/Colors.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** Colors
+*/
+
+#pragma once
+
+#define Blue "\033[0;34m"
+#define Green "\033[0;32m"
+#define Red "\033[0;31m"
+#define Yellow "\033[0;33m"
+#define Reset "\033[0m"
