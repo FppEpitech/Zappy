837262dd7b768afa8cc301e6cf9e31cf7d909011
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-06-11 18:08:26 +0200

    feat: new linemate asset

--- gui/include/Assets.hpp
+++ gui/include/Assets.hpp
@@ -24,7 +24,7 @@
 #define SIZE_TILE                   4.7
 
 #define SCALE_FOOD                  (Vector3){2, 2, 2}
-#define SCALE_LINEMATE              (Vector3){0.05, 0.05, 0.05}
+#define SCALE_LINEMATE              (Vector3){0.1, 0.1, 0.1}
 #define SCALE_MENDIANE              (Vector3){0.1, 0.1, 0.1}
 #define SCALE_PHIRAS                (Vector3){0.2, 0.2, 0.2}
 #define SCALE_SIBUR                 (Vector3){0.2, 0.2, 0.2}
@@ -54,7 +54,7 @@
 #define ROTATION_AXIS_EGG           (Vector3){1, 0, 0}
 
 #define POS_FOOD                    (Vector3){0.5, 0.1, 1.5}
-#define POS_LINEMATE                (Vector3){1, 0.1, -0.5}
+#define POS_LINEMATE                (Vector3){1, -0.3, -0.5}
 #define POS_MENDIANE                (Vector3){2, -0.25, -0.5}
 #define POS_PHIRAS                  (Vector3){0.5, 0.5, -1.5}
 #define POS_SIBUR                   (Vector3){1.5, 0.1, -1.5}
