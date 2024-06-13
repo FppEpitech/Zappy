32f14a38196ce057a22ef9eca55db376856c655b
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-06-13 17:13:27 +0200

    feat: display tile inventory stats in hud

--- gui/src/Hud/HudTile.cpp
+++ gui/src/Hud/HudTile.cpp
@@ -23,4 +23,14 @@ void Gui::HudTile::display()
     Vector2 hudPos = HUD_TILE_POS;
     DrawTexture(_texture, hudPos.x, hudPos.y, WHITE);
     DrawTextEx(_font, "   Tile", HUD_TILE_TEXT_TITLE_POS, 20, 0, (Color){170, 121, 89, 255});
+    if (_tile != nullptr) {
+        Vector2 hudTextPos = HUD_TILE_TEXT_POS;
+        DrawTextEx(_font, ("Food: " + std::to_string(_tile->inventory.getFood())).c_str(), (Vector2){hudTextPos.x, hudTextPos.y + HUD_TILE_TEXT_MARGING * 0}, 20, 0, WHITE);
+        DrawTextEx(_font, ("Linemate: " + std::to_string(_tile->inventory.getLinemate())).c_str(), (Vector2){hudTextPos.x, hudTextPos.y + HUD_TILE_TEXT_MARGING * 1}, 20, 0, WHITE);
+        DrawTextEx(_font, ("Deraumere: " + std::to_string(_tile->inventory.getDeraumere())).c_str(), (Vector2){hudTextPos.x, hudTextPos.y + HUD_TILE_TEXT_MARGING * 2}, 20, 0, WHITE);
+        DrawTextEx(_font, ("Mendiane: " + std::to_string(_tile->inventory.getMendiane())).c_str(), (Vector2){hudTextPos.x, hudTextPos.y + HUD_TILE_TEXT_MARGING * 3}, 20, 0, WHITE);
+        DrawTextEx(_font, ("Phiras: " + std::to_string(_tile->inventory.getPhiras())).c_str(), (Vector2){hudTextPos.x, hudTextPos.y + HUD_TILE_TEXT_MARGING * 4}, 20, 0, WHITE);
+        DrawTextEx(_font, ("Sibur: " + std::to_string(_tile->inventory.getSibur())).c_str(), (Vector2){hudTextPos.x, hudTextPos.y + HUD_TILE_TEXT_MARGING * 5}, 20, 0, WHITE);
+        DrawTextEx(_font, ("Thystame: " + std::to_string(_tile->inventory.getThystame())).c_str(), (Vector2){hudTextPos.x, hudTextPos.y + HUD_TILE_TEXT_MARGING * 6}, 20, 0, WHITE);
+    }
 }
