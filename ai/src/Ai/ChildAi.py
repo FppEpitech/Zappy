8fd8050ab3a4b9c036486fda7c6e80eb4c124477
Théophile JR <theophilejeromerocher44@gmail.com>
2024-06-14 16:53:44 +0200

    feat: move player to x tile

--- ai/src/Ai/ChildAi.py
+++ ai/src/Ai/ChildAi.py
@@ -1,12 +1,35 @@
 from ai.src.Player.Player import Player
 from ai.src.Enum.Item import Item
 
+def getXmovement(middle, max, width, target):
+    if middle == target:
+        return 0
+    return target - middle
+ 
+def getMovesTowardTile(index):
+    maxRowNum = 3
+    crowWidth = 3
+    fwdRow = 1
+    middleTileIndex = 2
+    
+    if index <= maxRowNum:
+        return (getXmovement(middleTileIndex, maxRowNum, crowWidth, index), 1)
+    for i in range(7):
+        fwdRow += 1
+        crowWidth += 2
+        middleTileIndex += fwdRow*2
+        maxRowNum += crowWidth
+        if index <= maxRowNum:
+            return (getXmovement(middleTileIndex, maxRowNum, crowWidth, index), fwdRow)
+    return -1
+
 def numXItemVision(vision : list, item : Item):
-       count = 0
-       for elem in vision:
-           if elem == item:
-               count += 1
-       return count
+    total : int = 0
+
+    for elem in vision:
+        if elem.food > 0:
+            total += elem.food
+    return total
 
 class ChildAi:
     player : Player = None
@@ -17,15 +40,39 @@ class ChildAi:
     def __init__(self, player):
         self.player = player
 
-    def updateFood(self):
+    def updateFoodVars(self):
         self.food = self.player.inventory.food
-        self.foodInSight = numXItemVision(self.player.vision, Item.FOOD)
-        print("food: ", self.food, "foodInSight: ", self.foodInSight)
+        self.foodInSight = numXItemVision(self.player.vision, "food")
+
+    def askDataUpdate(self):
+        self.player.cmdInventory()
+        self.queueCmd()
+        self.player.look()
+        self.queueCmd()
+        
+    def goTowardTile(self, index, itemSeek : Item):
+        (x, y) = getMovesTowardTile(index)
+
+        for i in x:
+            self.player.moveForward()
+            self.queueCmd()
+        if y > 0:
+            self.player.turnRight()
+            self.queueCmd()
+        if y < 0:
+            self.player.turnLeft()
+            self.queueCmd()
+        for i in y:
+            self.player.moveForward()
+            self.queueCmd()
+        self.player.take(str(itemSeek))
+        self.queueCmd()
 
     def computeAction(self):
-        self.updateFood()
+        self.updateFoodVars()
         self.player.randomMoove()
         self.queueCmd()
+        self.askDataUpdate()
 
     def queueCmd(self):
         self.player.queue.append((self.player.currentCommand, self.player.currentAction))
