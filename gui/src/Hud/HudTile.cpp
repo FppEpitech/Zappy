27bd4c151aead632da0ab404ca624ccd7a88f4cb
Mathieu Robert <mathieu1.robert@epitech.eu>
2024-06-20 20:58:49 +0200

    feat: display nb eggs on tile in Tile HUD

--- gui/src/Hud/HudTile.cpp
+++ gui/src/Hud/HudTile.cpp
@@ -25,6 +25,8 @@ Gui::HudTile::HudTile(std::shared_ptr<GameData> gameData)
     _thystame = LoadTexture(PNG_THYSTAME);
     _font = LoadFont(FONT_HUD);
     _tileTexture = LoadTexture(PNG_TILE);
+    _playerTexture = LoadTexture(PNG_PLAYER);
+    _eggTexture = LoadTexture(PNG_EGG);
 }
 
 void Gui::HudTile::display()
@@ -50,5 +52,42 @@ void Gui::HudTile::display()
             count++;
         }
         DrawTexture(_tileTexture, hudPos.x + HUD_TILE_TITLE_ICON_MARGING.x, hudPos.y + HUD_TILE_TITLE_ICON_MARGING.y, WHITE);
+        displayNbPlayers();
+        displayNbEggs();
     }
+}
+
+void Gui::HudTile::displayNbPlayers()
+{
+    Vector2 hudTextPos = HUD_TILE_TEXT_POS;
+    std::size_t nbPlayerOnTile = 0;
+
+    for (auto &team : _gameData.get()->getTeams()) {
+        for (auto &player : team.getPlayers()) {
+            if (player.getPosition() == _tile.get()->getPosition())
+                nbPlayerOnTile++;
+        }
+    }
+
+    DrawTextEx(_font, ("Players: " + std::to_string(nbPlayerOnTile)).c_str(), (Vector2){hudTextPos.x, hudTextPos.y + HUD_TILE_TEXT_MARGING * 7}, 20, 0, WHITE);
+    DrawTexture(_playerTexture, hudTextPos.x + HUD_TILE_ICONS_MARGING, hudTextPos.y + HUD_TILE_TEXT_MARGING * 7, WHITE);
+}
+
+void Gui::HudTile::displayNbEggs()
+{
+    Vector2 hudTextPos = HUD_TILE_TEXT_POS;
+    std::size_t nbEggOnTile = 0;
+
+    for (auto &egg : _gameData.get()->getServerEggs()) {
+        if (egg.getPosition() == _tile.get()->getPosition())
+            nbEggOnTile++;
+    }
+    for (auto &team : _gameData.get()->getTeams()) {
+        for (auto &egg : team.getEggs()) {
+            if (egg.getPosition() == _tile.get()->getPosition())
+                nbEggOnTile++;
+        }
+    }
+    DrawTextEx(_font, ("Eggs: " + std::to_string(nbEggOnTile)).c_str(), (Vector2){hudTextPos.x, hudTextPos.y + HUD_TILE_TEXT_MARGING * 8}, 20, 0, WHITE);
+    DrawTexture(_eggTexture, hudTextPos.x + HUD_TILE_ICONS_MARGING, hudTextPos.y + HUD_TILE_TEXT_MARGING * 8, WHITE);
 }
