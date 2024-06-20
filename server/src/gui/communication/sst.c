22c684a11da6ec70bdb05a1046b44c164d7676f2
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-06-20 19:50:20 +0200

    fix(server): sst command, checks the value of sst to avoid setting the frequency to 0

--- server/src/gui/communication/sst.c
+++ server/src/gui/communication/sst.c
@@ -27,10 +27,11 @@ void sst_response(gui_t *gui, app_t *app, char *line)
     }
     line += LEN_COMMAND_AND_SPACE;
     freq = parse_positive_int_arg(line);
-    if (freq == CODE_ERROR_INVALID_NUMBER) {
+    if (freq == CODE_ERROR_INVALID_NUMBER || freq == 0) {
         sbp_command(gui);
         return;
     }
+    printf("sst %d\n", freq);
     app->game->freq = freq;
     response = format_string("sst %d\n", freq);
     add_message(gui->list_messages, response);
