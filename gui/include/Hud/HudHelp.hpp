05f83b5848fc9b0b1695e322c03ca7fca6375c3c
Thomaltarix <thomas.boue@epitech.eu>
2024-06-21 14:10:32 +0200

    feat: Hud Menu class

---
+++ gui/include/Hud/HudHelp.hpp
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** HudHelp
+*/
+
+#pragma once
+
+#include "Hud/AHud.hpp"
+
+namespace Gui {
+
+    /**
+     * @brief Class to display help's HUD.
+     *
+     */
+    class HudHelp;
+};
+
+class Gui::HudHelp : public Gui::AHud {
+
+    public:
+
+        /**
+         * @brief Construct a new Hud Help object.
+         *
+         * @param gameData GameData class.
+         */
+        HudHelp(std::shared_ptr<GameData> gameData);
+
+        /**
+         * @brief Destroy the Hud Help object.
+         *
+         */
+        ~HudHelp() = default;
+
+        /**
+         * @brief Display Help Hud.
+         *
+         */
+        void display();
+
+    private:
+
+        Texture2D       _texture;           // Texture for Hud Background.
+        Font            _font;              // Font for Hud's texts.
+        clock_t         _helpMenuClock;     // Clock to display the help menu.
+
+        /**
+         * @brief Display Help Menu Controls.
+         *
+         * @param position Position of the help menu.
+         */
+        void displayHelpMenuControls();
+};
---
+++ gui/src/Hud/HudHelp.cpp
@@ -0,0 +1,52 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** HudHelp
+*/
+
+#include "Hud/HudHelp.hpp"
+#include "Assets.hpp"
+#include "Config.hpp"
+
+Gui::HudHelp::HudHelp(std::shared_ptr<GameData> gameData)
+{
+    _typeScene = TypeScene::HELP_MENU;
+    _gameData = gameData;
+    _texture = LoadTexture(PNG_HELP);
+    _font = LoadFont(FONT_HUD);
+    _helpMenuClock = clock();
+}
+
+void Gui::HudHelp::display()
+{
+    if (_typeScene == TypeScene::HELP_MENU) {
+        std::string help = "Press 'H' to close the help menu.";
+        DrawText(help.c_str(), WINDOW_WIDTH / 2 - MeasureText(help.c_str(), 20) / 2, 20, 20, WHITE);
+        std::cout << "help background position: " << HELP_BACKGROUND_POSITION.x << " " << HELP_BACKGROUND_POSITION.y << std::endl;
+        DrawTextureEx(_texture, HELP_BACKGROUND_POSITION, 0, HELP_BACKGROUND_SCALE, WHITE);
+        displayHelpMenuControls();
+    }
+    if (_typeScene == TypeScene::HELP_TEXT) {
+        std::string help = "Press 'H' to display the help menu.";
+        clock_t time = clock();
+        float timeElapsed = float(time - _helpMenuClock) / CLOCKS_PER_SEC * 1000;
+        if (timeElapsed > 50 && timeElapsed < 100)
+            DrawText(help.c_str(), WINDOW_WIDTH / 2 - MeasureText(help.c_str(), 20) / 2, 20, 20, WHITE);
+        if (timeElapsed >= 100)
+            _helpMenuClock = clock();
+    }
+}
+
+void Gui::HudHelp::displayHelpMenuControls()
+{
+    Vector2 position = HELP_TEXT_POSITION_LEFT;
+    std::cout << "position: " << position.x << " " << position.y << std::endl;
+    DrawText("Global Controls", position.x, position.y - HELP_TITLE_OFFSET, 40, WHITE);
+    for (size_t i = 0; i < globalControlsTexts.size(); i++)
+        DrawText(globalControlsTexts[i].c_str(), position.x, position.y + HELP_TEXT_SPACING * i, 20, WHITE);
+    position = HELP_TEXT_POSITION_RIGHT;
+    DrawText("Player Controls", position.x, position.y - HELP_TITLE_OFFSET, 40, WHITE);
+    for (size_t i = 0; i < playerControlsTexts.size(); i++)
+        DrawText(playerControlsTexts[i].c_str(), position.x, position.y + HELP_TEXT_SPACING * i, 20, WHITE);
+}
