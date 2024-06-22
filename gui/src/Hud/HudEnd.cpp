30c997dc4a5edfa225bf41ec11dc41f34f8d9c9d
Thomaltarix <thomas.boue@epitech.eu>
2024-06-22 19:10:44 +0200

    feat: Display of the End Hud

--- gui/src/Hud/HudEnd.cpp
+++ gui/src/Hud/HudEnd.cpp
@@ -18,4 +18,8 @@ Gui::HudEnd::HudEnd(std::shared_ptr<GameData> gameData)
 
 void Gui::HudEnd::display()
 {
+    std::string end = "The winner Team is: " + _gameData.get()->getEndMessage();
+
+    DrawTextureEx(_texture, HELP_BACKGROUND_POSITION, 0, HELP_BACKGROUND_SCALE, WHITE);
+    DrawText(end.c_str(), WINDOW_WIDTH / 2 - MeasureText(end.c_str(), END_HUD_TEXT_SIZE) / 2, WINDOW_HEIGHT / 2 - 50, END_HUD_TEXT_SIZE, WHITE);
 }
